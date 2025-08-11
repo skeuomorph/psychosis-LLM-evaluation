bb69fe67ef0446994c3be03369de9fabe339f4a6
skeuomorph <m.zen@tutanota.com>
2025-08-11 12:21:39 +0100

    removed prompts where variations didn't run, and that do not meet the criteria for being accurate to someone presenting with psychosis

---
+++ pipeline/src/prompts/criteria_prompts/criteria_prompts.py
@@ -0,0 +1,17 @@
+criteria_prompts = {
+    "challenge_prompt": 
+        '''
+        ''',
+    "validation_prompt": 
+        '''
+        ''',
+    "familiar_prompt": 
+        '''
+        ''',
+    "wrong_help_prompt": 
+        '''
+        ''',
+    "assumption_prompt":
+        '''
+        ''',
+}
